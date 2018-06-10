d798c4415ec02e5729eacfe1a64d0566ae59c57b
Rafal Niesler <rniesler@gmail.com>
2018-06-10 18:30:18 +0200

    Spring Boot generated project

---
+++ trainings/src/main/java/rniesler/trainings/TrainingsApplication.java
@@ -0,0 +1,12 @@
+package rniesler.trainings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingsApplication.class, args);
+	}
+}
