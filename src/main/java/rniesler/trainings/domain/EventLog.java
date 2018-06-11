096de269eed6937316d3c635bf7497eca28fd850
Rafal Niesler <rniesler@gmail.com>
2018-06-11 14:45:59 +0200

    Added event log db migration

--- src/main/java/rniesler/trainings/domain/EventLog.java
+++ src/main/java/rniesler/trainings/domain/EventLog.java
@@ -1,4 +1,19 @@
 package rniesler.trainings.domain;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import java.util.UUID;
+
+@Entity
+@Data
 public class EventLog {
+    private UUID aggregateId;
+    private Integer version;
+    private String data;
+    private String type;
+
+    public EventLog() {
+        //make JPA happy
+    }
 }
---
+++ src/main/resources/db/migration/V0_2__eventlog.sql
@@ -0,0 +1,15 @@
+create table event_log (
+  aggregate_id BINARY(16) NOT NULL,
+  aggregate_id_text varchar(36) generated always as
+   (insert(
+      insert(
+        insert(
+          insert(hex(aggregate_id),9,0,'-'),
+          14,0,'-'),
+        19,0,'-'),
+      24,0,'-')
+   ) virtual,
+  version INT NOT NULL,
+  type VARCHAR(100) NOT NULL,
+  data VARCHAR(9999) NOT NULL,
+  FOREIGN KEY (aggregate_id) REFERENCES aggregate(id));
